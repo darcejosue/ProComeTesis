91d05ef53dc0ab21fc11b2c896a0110a41e65398
Josue M Darce <133271041+darcejosue@users.noreply.github.com>
2024-09-27 19:31:14 -0600

    firt commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "ProCome",
+  description: "Sistema Web de Informacion sobre el comedor DElicias La Tia",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Comedor from "../components/ComedorMain";
+
+export default function Home() {
+  return (
+    <div >
+      <Comedor/>
+    </div>
+  );
+}
